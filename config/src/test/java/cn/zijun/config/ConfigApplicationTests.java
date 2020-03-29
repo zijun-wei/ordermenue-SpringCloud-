da38c63ff5b3de4e41510972381e7fca82529918
zijun-wei <weijiaqiqi@126.com>
2020-03-29 12:19:56 +0800

    配置中心

---
+++ config/src/test/java/cn/zijun/config/ConfigApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.zijun.config;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
