b1b4ac8eeb1a01d1e62fe848b3bc2b9d4a69984e
zijun-wei <weijiaqiqi@126.com>
2020-03-29 12:22:28 +0800

    注册中心

---
+++ eureka/src/main/java/cn/zijun/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.zijun.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.util.Base64Utils;
+
+import java.util.Base64;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ eureka/src/test/java/cn/zijun/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zijun.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
